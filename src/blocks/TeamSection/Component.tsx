736bfb43b8c3b94d9bf562ad7ac075d90a085779
sanjayguwaju <sanjayguwaju@gmail.com>
2024-12-13 10:07:46 +0545

    feat: add new content and layout enhancements to TeamSection component

--- src/blocks/TeamSection/Component.tsx
+++ src/blocks/TeamSection/Component.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-export const TeamSection:React.FC<TeamSectionProps> = (props) => {
+export const TeamSection: React.FC<TeamSectionProps> = (props) => {
   const { introContent, media } = props
   return (
     <>
@@ -60,6 +60,106 @@ export const TeamSection:React.FC<TeamSectionProps> = (props) => {
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+        <div>
+          <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <h2 className="text-center text-lg/8 font-semibold text-gray-900">
+                Trusted by the world’s most innovative teams
+              </h2>
+              <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                <img
+                  alt="Transistor"
+                  src="https://tailwindui.com/plus/img/logos/158x48/transistor-logo-gray-900.svg"
+                  width={158}
+                  height={48}
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                />
+                <img
+                  alt="Reform"
+                  src="https://tailwindui.com/plus/img/logos/158x48/reform-logo-gray-900.svg"
+                  width={158}
+                  height={48}
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                />
+                <img
+                  alt="Tuple"
+                  src="https://tailwindui.com/plus/img/logos/158x48/tuple-logo-gray-900.svg"
+                  width={158}
+                  height={48}
+                  className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                />
+                <img
+                  alt="SavvyCal"
+                  src="https://tailwindui.com/plus/img/logos/158x48/savvycal-logo-gray-900.svg"
+                  width={158}
+                  height={48}
+                  className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                />
+                <img
+                  alt="Statamic"
+                  src="https://tailwindui.com/plus/img/logos/158x48/statamic-logo-gray-900.svg"
+                  width={158}
+                  height={48}
+                  className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="max-w-screen-md mb-8 lg:mb-16 mx-auto text-center">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+              Designed for business teams like yours
+            </h2>
+            <p className="text-gray-500 sm:text-xl dark:text-gray-400">
+              Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+            </p>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded-lg overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+              <img className="w-full" src="https://via.placeholder.com/600x400" alt="Marketing" />
+              <div className="px-6 py-4">
+                <h3 className="mb-2 text-xl font-bold dark:text-white">Study in UK</h3>
+                <p className="text-gray-500 dark:text-gray-400">Learn more..</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
