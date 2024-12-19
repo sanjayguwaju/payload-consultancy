02f7d362ea58149fd9e572aa56e91b4fc5a2dcaa
Tulce <tulsi.gautam0000@gmail.com>
2024-12-19 23:36:37 +0545

    ReviewType2 variant is implemented

--- src/blocks/Review/variants/ReviewType2.tsx
+++ src/blocks/Review/variants/ReviewType2.tsx
@@ -1,14 +1,48 @@
-
-import Image from 'next/image'
+import RichText from '@/components/RichText'
 import React from 'react'
 
 export const ReviewType2 = (props: any) => {
-  const { introContent, media } = props
+  const { introContent, media, reviews } = props
   return (
     <>
-      <section className="bg-white dark:bg-gray-900">
-        <div>
-          <h1>Review Section 2</h1>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm mb-8 lg:mb-16">
+            <RichText
+              className="text-lg font-medium text-gray-800 dark:text-white"
+              content={introContent}
+              enableGutter={false}
+            />
+          </div>
+          <div className="grid gap-8 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-3">
+            {reviews &&
+              reviews?.map((review: any, index: number) => (
+                <div
+                  key={index}
+                  className="p-6 bg-amber-50 rounded-2xl shadow dark:bg-gray-800 relative overflow-hidden"
+                >
+                  <div className="mb-4 text-indigo-500">
+                    {/* <svg
+                      aria-hidden="true"
+                      className="w-8 h-8 mx-auto"
+                      fill="currentColor"
+                      viewBox="0 0 24 27"
+                    >
+                      <path d="M22 0H2C0.9 0 0 0.9 0 2v24c0 1.1 0.9 2 2 2h20c1.1 0 2-0.9 2-2V2c0-1.1-0.9-2-2-2zm-9 21h-2v-2h2v2zm4.28-9.49l-3.8 3.56c-.39.36-.89.55-1.42.55h-.11v-2h.11l3.8-3.56c.77-.73 2.05-.71 2.78.07.75.76.77 2.04.03 2.81z" />
+                    </svg> */}
+                  </div>
+                  <blockquote className="text-gray-600 dark:text-gray-300">
+                    <p>{review?.content}</p>
+                  </blockquote>
+                  <div className="mt-6 text-center">
+                    <p className="font-bold text-gray-800 dark:text-white">{review?.author}</p>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">{review?.location}</p>
+                  </div>
+                  {/* Folded Corner */}
+                  <div className="absolute bottom-0 right-0 w-10 h-10 bg-gray-200 dark:bg-gray-700 transform rotate-45 origin-bottom-left"></div>
+                </div>
+              ))}
+          </div>
         </div>
       </section>
     </>
