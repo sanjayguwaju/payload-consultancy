1574c73dede0e9f506e2872bbd7a30fbe80d1a5b
sanjayguwaju <sanjayguwaju@gmail.com>
2024-12-26 20:22:57 +0545

    feat: add FeatureType2 variant to FeatureBlock and update variants mapping

--- src/blocks/FeatureBlock/Component.tsx
+++ src/blocks/FeatureBlock/Component.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { FeatureType1 } from './variants/FeatureType1'
+import { FeatureType2 } from './variants/FeatureType2'
 
 
 const featureVariants = {
   featureType1: FeatureType1,
+  featureType2: FeatureType2,
 }
 
 export const FeatureBlock = (props: any) => {
---
+++ src/blocks/FeatureBlock/variants/FeatureType2.tsx
@@ -0,0 +1,14 @@
+import RichText from '@/components/RichText'
+import Image from 'next/image'
+import React from 'react'
+
+export const FeatureType2 = (props: any) => {
+  const { id, introContent, media } = props
+  return (
+    <>
+      <div>
+        <h1>This is feature block 2</h1>
+      </div>
+    </>
+  )
+}
